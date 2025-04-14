2554c8064e42950c1192906e380d465f1b258a9f
heliobrandao <heliob79@gmail.com>
2025-04-14 19:32:22 -0300

    first commit

---
+++ src/test/java/br/edu/uftpr/cp/espjava/crud_cidades/CrudCidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uftpr.cp.espjava.crud_cidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
