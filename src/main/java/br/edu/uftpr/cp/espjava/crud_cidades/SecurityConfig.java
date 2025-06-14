15c18d14543d838d77712f18aa3980344dfb3159
heliobrandao <heliob79@gmail.com>
2025-06-13 22:32:20 -0300

    semana07-4-autenticacao-db

--- src/main/java/br/edu/uftpr/cp/espjava/crud_cidades/SecurityConfig.java
+++ src/main/java/br/edu/uftpr/cp/espjava/crud_cidades/SecurityConfig.java
@@ -2,35 +2,16 @@ package br.edu.uftpr.cp.espjava.crud_cidades;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
-    @Bean
-    public InMemoryUserDetailsManager configure() throws Exception {
-        UserDetails john = User.withUsername("john")
-                    .password(cifrador().encode("test123"))
-                    .roles("listar")
-                    .build();
-
-        UserDetails anna = User.withUsername("anna")
-                    .password(cifrador().encode("test123"))
-                    .roles("admin")
-                    .build();
-
-        return new InMemoryUserDetailsManager(john, anna);
-    }
 
     @Bean
     protected SecurityFilterChain filter(HttpSecurity http) throws Exception {
---
+++ src/main/java/br/edu/uftpr/cp/espjava/crud_cidades/usuario/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.edu.uftpr.cp.espjava.crud_cidades.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+    public Usuario findByNome(String nome);
+}
